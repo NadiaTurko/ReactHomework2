89d9d2a1712d1ab9a789891bd7ebf05fe54e56bd
NadiaTurko <47818620+NadiaTurko@users.noreply.github.com>
2023-08-26 20:16:47 +0300

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,24 @@
+import React, { Component } from "react";
+import Table from "./components/Table/Table";
+
+class App extends Component {
+  state = {
+    animals: [
+      { type: `turtle`, icon: `🐢` },
+      { type: `octopus`, icon: `🐙` },
+      { type: `fish`, icon: `🐠` },
+      { type: `flamingo`, icon: `🦩` },
+      { type: `penguin`, icon: `🐧` },
+    ],
+  };
+  render() {
+    let { animals } = this.state;
+    return (
+      <>
+        <Table list={animals} />
+      </>
+    );
+  }
+}
+
+export default App;
